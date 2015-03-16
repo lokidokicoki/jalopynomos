0bb30e19af6f4fb52318037dbd7b66ab8a033dbe
rtregaskis <rtregaskis@dresources.com>
2015-03-16 20:09:32 +0000

    renamed parseDate helper as formatDate. new `ifeq` helper to compare to values

--- index.js
+++ index.js
@@ -12,47 +12,55 @@ var oneDay = 86400000;
 
 api.load(data);
 
-hbs.registerHelper('getFuelType', function(type){
-	return api.getFuelType(type);
+hbs.registerHelper('getFuelType', function(type) {
+    return api.getFuelType(type);
 });
-hbs.registerHelper('parseDate', function(dateString) {
-	return utils.parseDate(dateString);
+hbs.registerHelper('formatDate', function(dateString) {
+    return utils.formatDate(dateString);
 });
 
 hbs.registerHelper('formatCost', function(cost, options) {
-	return utils.formatCost(cost, options);
+    return utils.formatCost(cost, options);
 });
 
 hbs.registerHelper('formatMPG', function(mpg) {
-	return mpg.toFixed(2);
+    return mpg.toFixed(2);
 });
 
-hbs.registerHelper('fuelSummary', function(id){
-	return api.getFillUp(id).toString();
+hbs.registerHelper('fuelSummary', function(id) {
+    return api.getFillUp(id).toString();
 });
 
-hbs.registerHelper('serviceSummary', function(id){
-	return api.getService(id).toString();
+hbs.registerHelper('serviceSummary', function(id) {
+    return api.getService(id).toString();
 });
 
-app.set( 'views', path.join( __dirname, 'views' ));
+hbs.registerHelper('ifeq', function(a, b, options) {
+    if (a == b) {
+        return options.fn(this);
+    }
+    return options.inverse(this);
+});
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(partials()); // use partials for layout.html
 // use bodyParser
 app.use(bodyParser.urlencoded({
-	extended:false
+    extended: false
 }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(express.static('public', { maxAge:oneDay }));
+app.use(express.static('public', {
+    maxAge: oneDay
+}));
 
 
-var server = app.listen('51000', function (){
-	var port = server.address().port;
-	console.log('express @ %s', port);
+var server = app.listen('51000', function() {
+    var port = server.address().port;
+    console.log('express @ %s', port);
 });
 require('./routes')(app, api);
-
