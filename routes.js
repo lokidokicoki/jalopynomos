f541b426b741d4e847fd0459aceefa74402f12d3
rtregaskis <rtregaskis@dresources.com>
2015-05-09 19:16:38 +0100

    conflate `about` and `contact` pages. include version number on about.

--- routes.js
+++ routes.js
@@ -1,14 +1,15 @@
 var utils = require('./server/utils');
+var pkg = require('./package.json');
 module.exports = function(app, api) {
 	// menu bar routes
 	app.get('/', function (req, res){
 		res.render('index', {vehicles:api.getVehicleArray(), title:'Jalopynomos'});
 	});
 	app.get('/about', function(req, res) {
-		res.render('misc/about', {title:'About'});
-	});
-	app.get('/contact', function(req, res) {
-		res.render('misc/contact', {title:'Contact'});
+		res.render('misc/about', {
+			title:'About',
+			version:pkg.version
+		});
 	});
 	app.get('/history', function(req, res) {
 		res.render('misc/history', {title:'Historic Fuel Prices'});
