aeaf6e141173fef9f0a113c0d6b811a0c226d276
rtregaskis <rtregaskis@dresources.com>
2015-06-03 21:03:42 +0100

    refactor summary api. gridify vehicle details

--- server/api.js
+++ server/api.js
@@ -129,23 +129,36 @@ function Summary(vehicle) {
             max: 0,
             avg: 0
         };
-        this.fuelCost = 0;
-        this.serviceCost = 0;
-        this.mileage = 0;
-        this.totalCost = 0;
-        this.runningCost = 0;
-        this.totalCostPerMile = 0;
-        this.runningCostPerMile = 0;
-		this.milesPerDay = 0;
-		this.milesPerYear = 0;
-		this.predictedMilesPerDay = 0;
-		this.predictedMilesPerYear = 0;
+		this.range = {
+			min:0,
+			max:0,
+			avg:0
+		};
+		this.costs ={
+			fuel:0,
+			service:0,
+			total:0,
+			running:0,
+			distance:{
+				total:0,
+				running:0
+			}
+		};
+		this.distance = {
+			total:0,
+			daily:0,
+			yearly:0,
+			predicted:{
+				daily:0,
+				yearly:0
+			}
+		};
 		this.lastRecordDate = 0;
     };
 
     this.summarise = function() {
         this.reset();
-        this.totalCost = this.vehicle.purchase.price;
+        this.costs.total = this.vehicle.purchase.price;
 
         var recs = this.vehicle.fuelRecs,
             len = 0,
@@ -155,15 +168,15 @@ function Summary(vehicle) {
             len = recs.length;
             for (; i < len; i++) {
                 rec = recs[i];
-                this.totalCost += rec.cost;
-                this.fuelCost += rec.cost;
+                this.costs.total += rec.cost;
+                this.costs.fuel += rec.cost;
                 this.mpg.max = Math.max(this.mpg.max, rec.mpg);
                 this.mpg.min = Math.min(this.mpg.min, rec.mpg);
                 this.mpg.avg += rec.mpg;
                 this.ppl.max = Math.max(this.ppl.max, rec.ppl);
                 this.ppl.min = Math.min(this.ppl.min, rec.ppl);
                 this.ppl.avg += rec.ppl;
-				this.mileage += rec.trip;
+				this.distance.total += rec.trip;
 				this.lastRecordDate = Math.max(this.lastRecordDate, rec.date);
             }
 
@@ -177,23 +190,23 @@ function Summary(vehicle) {
             i = 0;
             for (; i < len; i++) {
                 rec = recs[i];
-                this.totalCost += rec.cost;
-                this.serviceCost += rec.cost;
+                this.costs.total += rec.cost;
+                this.costs.service += rec.cost;
             }
         }
 		
-		this.runningCost = this.serviceCost + this.fuelCost;
-		this.runningCostPerMile = this.runningCost / this.mileage;
-		this.totalCostPerMile = this.totalCost / this.mileage;
+		this.costs.running = this.costs.service + this.costs.fuel;
+		this.costs.distance.running = this.costs.running / this.distance.total;
+		this.costs.distance.total = this.costs.total / this.distance.total;
     };
 
 	this.update = function(now){
-		var a = (this.mileage / (this.lastRecordDate - this.vehicle.purchase.date));
-		var p = (this.mileage / (now - this.vehicle.purchase.date));
-		this.milesPerDay  = a * DAY_IN_MS;
-		this.milesPerYear  = a * YEAR_IN_MS;
-		this.predictedMilesPerDay  = p * DAY_IN_MS;
-		this.predictedMilesPerYear  = p * YEAR_IN_MS;
+		var a = (this.distance.total / (this.lastRecordDate - this.vehicle.purchase.date));
+		var p = (this.distance.total / (now - this.vehicle.purchase.date));
+		this.distance.daily  = a * DAY_IN_MS;
+		this.distance.yearly  = a * YEAR_IN_MS;
+		this.distance.predicted.daily  = p * DAY_IN_MS;
+		this.distance.predicted.yearly  = p * YEAR_IN_MS;
 	};
 }
 
