4025f691b357619c075606599a6c6c45de07804c
lokidokicoki <lokidokicoki@gmail.com>
2016-12-26 14:39:38 +0000

    feat: first use of flow annotations

--- src/server/fuel.js
+++ src/server/fuel.js
@@ -1,34 +1,54 @@
 'use strict';
+/* @flow */
 import _ from 'lodash';
 import * as U from './utils';
 import * as api from './api';
 /**
- * @param {object} values new fuel record values
- * @constructor
+ * Holder for fuel records
+ * @class Fuel
  */
 class Fuel {
-  constructor(values) {
-    this.id = null;
-    this.date = ``;
+  id: number;
+  date: string;
+  litres: number;
+  ppl: number;
+  trip: number;
+  odo: number;
+  cost: number;
+  mpg: number;
+  notes: string;
+  type: string;
+
+  /**
+   * @param {object} values new fuel record values
+   * @constructor
+   */
+  constructor(values: Object) {
+    this.id     = -1;
+    this.date   = ``;
     this.litres = 0;
-    this.ppl = 0;
-    this.trip = 0;
-    this.odo = 0;
-    this.cost = 0;
-    this.mpg = 0;
-    this.notes = ``;
-    this.type = `U`;
+    this.ppl    = 0;
+    this.trip   = 0;
+    this.odo    = 0;
+    this.cost   = 0;
+    this.mpg    = 0;
+    this.notes  = ``;
+    this.type   = `U`;
 
     // copy constructor
-    for (let key in values) {
+    _.forEach(values, (item, key) => {
       this[key] = values[key];
-    }
+    });
 
     if (this.id === null || this.id === undefined) {
       this.id = _.size(api.getFillUps()) + 1;
     }
   }
 
+  /**
+   * Get record as string
+   * @return {string}
+   */
   toString() {
     // date | cost | litres | trip | odo | mpg
     let data = U.formatDate(this.date) + ` | ` +
@@ -40,13 +60,19 @@ class Fuel {
     return data;
   }
 
+  /**
+   * Calculate MPG for this record
+   */
   calculateMPG() {
     this.mpg = this.trip / (this.litres / U.LITRES_IN_GALLON);
   }
 
+  /**
+   * Calculate price per litre for this record
+   */
   calculatePPL() {
     this.ppl = parseFloat((this.cost / this.litres).toFixed(3));
   }
 }
 
-export { Fuel as default };
+export {Fuel as default};
--- src/server/service.js
+++ src/server/service.js
@@ -1,31 +1,47 @@
 'use strict';
-
+/* @flow */
 import _ from 'lodash';
 import * as utils from './utils';
 import * as api from './api';
+
 /**
- * @param {object} values new service record values
- * @constructor
+ * Service record holder
+ * @class Sevice
  */
 class Service {
-  constructor(values) {
-    this.id = null;
-    this.date = ``;
-    this.cost = 0;
-    this.odo = 0;
-    this.item = ``;
+  id: number;
+  date: string;
+  cost: number;
+  odo: number;
+  item: string;
+  notes: string;
+
+  /**
+   * @param {object} values new service record values
+   * @constructor
+   */
+  constructor(values: Object) {
+    this.id    = -1;
+    this.date  = ``;
+    this.cost  = 0;
+    this.odo   = 0;
+    this.item  = ``;
     this.notes = ``;
 
     // copy constructor
-    for (let key in values) {
+    _.forEach(values, (item: any, key: string) => {
       this[key] = values[key];
-    }
+    });
 
     if (this.id === null || this.id === undefined) {
       this.id = _.size(api.getServices()) + 1;
     }
   }
 
+  /**
+   * Get record as string
+   * @return {string}
+   */
   toString() {
     // date | cost | litres | trip | odo | mpg
     let data = utils.formatDate(this.date) + ` | ` +
@@ -37,4 +53,4 @@ class Service {
   }
 }
 
-export { Service as default };
+export {Service as default};
